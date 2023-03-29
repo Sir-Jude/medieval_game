5f4ea64472de990656f820869f27bc23bd5a48d1
Giulio Arpaia <giulio.arpaia@gmail.com>
2023-03-29 11:24:27 +0200

    modify Character file

--- armor.py
+++ armor.py
@@ -2,23 +2,23 @@ from abc import ABC
 
 
 class Armor(ABC):
-    def __init__(self, name, defence, armor_type):
+    def __init__(self, name, defence, skill):
         self.name = name
         self.defence = defence
-        self.armor_type = armor_type
+        self.armor_type = skill
 
 def armor_factory(armor):
     if armor.lower() == "cloack":
-        return Armor("cloack", 5, "light")
+        return Armor("cloack", 5, "magic")
     elif armor.lower() == "helmet":
-        return Armor("helmet", 10, "heavy")
+        return Armor("helmet", 10, "distant")
     elif armor.lower() == "round shield":
-        return Armor("round shield", 15, "light")
+        return Armor("round shield", 15, "close")
     elif armor.lower() == "roman shield":
-        return Armor("roman shield", 20, "heavy")
+        return Armor("roman shield", 20, "close")
     elif armor.lower() == "leather armor":
-        return Armor("leather armor", 15, "light")
+        return Armor("leather armor", 15, "distant")
     elif armor.lower() == "metal armor":
-        return Armor("metal armor", 20, "heavy")
+        return Armor("metal armor", 20, "close")
     else:
         raise ValueError(f"Invalid armor type: {armor}")
--- character.py
+++ character.py
@@ -66,91 +66,6 @@ class Character:
         return f"Name: {self.name}, the Great\nHealth: {self.health}\nDamage: {((self.weapon).damage)}"
 
 
-class Magician(Character):
-    def __init__(self, name, health, weapon, armor=None):
-        super().__init__(name, health)
-        self.weapon = weapon
-        self.armor = armor
-        self.defence_points = 10
-        self.attack_points = 10
-
-    def attack(self, weapon):
-        self.distraction = int(
-            random(0, 11)
-        )  # distraction can take til 100% from attack_points
-        if weapon:
-            if weapon.skill == "Spell":
-                self.attack_points += weapon.damage - self.distraction + 5
-                # a proper weapon can add til 50% from attack_points
-            elif weapon.skill == "Sword":
-                self.attack_points += weapon.damage - self.distraction - 5
-                # wrong weapon can take til 50% from attack_points
-            elif weapon.skill == "Bow":
-                self.attack_points += weapon.damage - self.distraction
-                # wrong weapon can take til 50% from attack_points
-        return self.attack_points
-
-    def defend(self, armor):
-        self.distraction = int(
-            random(0, 11)
-        )  # distraction can take til 100% from defence_points
-        if armor:
-            if armor.skill == "Shield":
-                self.defence_points += armor.defence - self.distraction
-                # wrong armor can take til 50% from defence_points
-            elif armor.skill == "Cloak":
-                self.defence_points += armor.defence - self.distraction + 5
-                # a proper armor can add til 50% from defence_points
-            elif armor.skill == "Helmet":
-                self.defence_points += armor.defence - self.distraction - 5
-                # wrong armor can take til 50% from defence_points
-        return self.defence_points
-
-    def get_info(self):
-        return f"Name: {self.name}, the Mage\nHealth: {self.health}\nDamage: {((self.weapon).damage)}"
-
-
-class Archer(Character):
-    def __init__(self, name, health, weapon, armor=None):
-        super().__init__(name, health)
-        self.weapon = weapon
-        self.armor = armor
-        self.defence_points = 10
-        self.attack_points = 10
-
-    def attack(self, weapon):
-        self.distraction = int(
-            random(0, 11)
-        )  # distraction can take til 100% from attack_points
-        if weapon:
-            if weapon.skill == "Spell":
-                self.attack_points += weapon.damage - self.distraction - 5
-                # a proper weapon can add til 50% from attack_points
-            elif weapon.skill == "Sword":
-                self.attack_points += weapon.damage - self.distraction + 4
-                # wrong weapon can take til 50% from attack_points
-            elif weapon.skill == "Bow":
-                self.attack_points += weapon.damage - self.distraction + 10
-                # wrong weapon can take til 50% from attack_points
-        return self.attack_points
-
-    def defend(self, armor):
-        self.distraction = int(
-            random(0, 11)
-        )  # distraction can take til 100% from defence_points
-        if armor:
-            if armor.skill == "Shield":
-                self.defence_points += armor.defence - self.distraction + 10
-                # wrong armor can take til 50% from defence_points
-            if armor.skill == "Cloak":
-                self.defence_points += armor.defence - self.distraction + 3
-                # a proper armor can add til 50% from defence_points
-            if armor.skill == "Helmet":
-                self.defence_points += armor.defence - self.distraction - 3
-                # wrong armor can take til 50% from defence_points
-        return self.defence_points
-
-
 def character_factory(character):
     if character.lower() == "warrior":
         return Character("warrior", name=None, health=10)
@@ -158,5 +73,5 @@ def character_factory(character):
         return Character("magician", name=None, health=10)
     elif character.lower() == "archer":
         return Character("archer", name=None, health=10)
-    elif character.lower() == "smith":
+    elif character.lower() == "blacksmith":
         return Character("smith", name=None, health=10)
