68d7f519a9cb4d83855ca3077fc6bc0c743bf90f
semen_zamai <zamay.sv@gmail.com>
2023-03-29 10:25:01 +0200

    armor refactoring3

--- armor.py
+++ armor.py
@@ -9,16 +9,16 @@ class Armor(ABC):
 
 def armor_factory(armor):
     if armor.lower() == "cloack":
-        return Armor("Cloack", 5, "light")
+        return Armor("cloack", 5, "light")
     elif armor.lower() == "helmet":
-        return Armor("Helmet", 10, "heavy")
+        return Armor("helmet", 10, "heavy")
     elif armor.lower() == "round shield":
-        return Armor("Round Shield", 15, "light")
+        return Armor("round shield", 15, "light")
     elif armor.lower() == "roman shield":
-        return Armor("Roman Shield", 20, "heavy")
+        return Armor("roman shield", 20, "heavy")
     elif armor.lower() == "leather armor":
-        return Armor("Leather Armor", 15, "light")
+        return Armor("leather armor", 15, "light")
     elif armor.lower() == "metal armor":
-        return Armor("Metal Armor", 20, "heavy")
+        return Armor("metal armor", 20, "heavy")
     else:
         raise ValueError(f"Invalid armor type: {armor}")
